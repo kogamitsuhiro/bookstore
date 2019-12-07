8f13f1df0e6fbdba8d35dfb86df8bae265af86d6
kogamitsuhiro <utirn.ame@gmail.com>
2019-12-07 14:40:19 +0900

    first commit

---
+++ front/components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>こんにちは</p>
+  </div>
+</template>
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
